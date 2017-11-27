df3bae7c9e3ea3629d8c9148b83285ef3968412c
martinnaughton <martin.oneachtain@gmail.com>
2017-11-27 12:59:02 +0000

    PL-114: added report model details

--- app/Report.php
+++ app/Report.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Report extends Model
 {
     //
+    protected $fillable = [ 
+         'name',
+         'user',
+         'errors',
+         'warnings',
+         'passes',
+         'qualityScore',
+         'created_at',
+         'updated_at'
+    ];  
 }
