f384a30f094c06e9cd84b0054234a331fbc4faf0
martinnaughton <martin.oneachtain@gmail.com>
2018-01-09 14:44:18 +0000

    Cleaned up migration and seed file locations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $this->call(Lintol\Capstone\Seeds\ProcessorsTableSeeder::class);
         $this->call(Seeders\RolesPermissionsSeeder::class);
     }
 }
