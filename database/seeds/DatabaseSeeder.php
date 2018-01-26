c46f53803c108edf5f96962e28f63725b9f8a603
martinnaughton <martin.oneachtain@gmail.com>
2018-01-26 11:58:38 +0000

    Added reports seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,14 +11,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(Lintol\Capstone\Seeds\ProcessorsTableSeeder::class,
-        Seeders\RolesPermissionsSeeder::class);
+        $this->call(Lintol\Capstone\Seeds\ProcessorsTableSeeder::class);
+        $this->call(Seeders\RolesPermissionsSeeder::class);
+        $this->call(Seeders\Sample\UserSeeder::class);
         $this->command->info("Set up Data seeded");
         
         /* Sample Data */ 
-        /* $this->call(Lintol\Capstone\Seeds\Sample\ProfilesTableSeeder::class,
-        Lintol\Capstone\Seeds\Sample\ReportsTableSeeder::class);
+        $this->call(Lintol\Capstone\Seeds\Sample\ProfilesTableSeeder::class);
+        $this->call(Lintol\Capstone\Seeds\Sample\ReportsTableSeeder::class);
         $this->command->info("Sample Data Seeded");
-        */
+        
     }
 }
--- packages/lintol/capstone/src/Seeds/Sample/ReportsTableSeeder.php
+++ packages/lintol/capstone/src/Seeds/Sample/ReportsTableSeeder.php
@@ -18,33 +18,155 @@ class ReportsTableSeeder extends Seeder
         $dataOwner = User::whereEmail('do@example.com')->first();
 
         $report = Report::firstOrNew([
-            'name' => 'Data Profile Name [test]',
+            'name' => 'Data Profile Name [test1]',
         ]);
         $report->fill([
             'errors' => '7',
             'warnings' => '44',
             'passes' => '44',
             'quality_score' => '76',
-            'content' => []
+            'content' => '{
+               "table-count" : 1,
+               "warnings" : [],
+               "error-count" : 3,
+               "tables" : [
+                  {
+                     "schema" : null,
+                     "time" : 0.032,
+                     "source" : "data/awful.csv",
+                     "encoding" : "utf-8",
+                     "scheme" : "file",
+                     "errors" : [
+                        {
+                          "processor": "csvlint",
+                           "code" : "duplicate-header",
+                           "row-number" : null,
+                           "message" : "Header in column 4 is duplicated to header in column(s) 2",
+                           "row" : null,
+                           "column-number" : 4
+                        },
+                        {
+                          "processor": "goodtables",
+                           "row" : [
+                              "4",
+                              "Salad",
+                              "3"
+                           ],
+                           "message" : "Row 5 has a missing value in column 4",
+                           "column-number" : 4,
+                           "code" : "missing-value",
+                           "row-number" : 5
+                        },
+                        {
+                          "processor": "goodtables",
+                           "column-number" : 5,
+                           "message" : "Row 5 has a missing value in column 5",
+                           "row" : [
+                              "4",
+                              "Salad",
+                              "3"
+                           ],
+                           "row-number" : 5,
+                           "code" : "missing-value"
+                        }
+                     ],
+                     "format" : "csv",
+                     "row-count" : 5,
+                     "valid" : false,
+                     "headers" : [
+                        "ID",
+                        "Mean",
+                        "N",
+                        "Mean",
+                        "Standard Deviation"
+                     ],
+                     "error-count" : 3
+                  }
+               ],
+               "preset" : "table",
+               "valid" : false,
+               "time" : 0.035
+            }'
         ]);
         $report->owner()->associate($dataOwner);
         $report->save();
 
         $report = Report::firstOrNew([
-            'name' => 'Data Profile Name [test]',
+            'name' => 'Data Profile Name [test2]',
         ]);
         $report->fill([
             'errors' => '7',
             'warnings' => '44',
             'passes' => '10',
             'quality_score' => '60',
-            'content' => []
+            'content' => '{
+               "table-count" : 1,
+               "warnings" : [],
+               "error-count" : 3,
+               "tables" : [
+                  {
+                     "schema" : null,
+                     "time" : 0.032,
+                     "source" : "data/awful.csv",
+                     "encoding" : "utf-8",
+                     "scheme" : "file",
+                     "errors" : [
+                        {
+                          "processor": "csvlint",
+                           "code" : "duplicate-header",
+                           "row-number" : null,
+                           "message" : "Header in column 4 is duplicated to header in column(s) 2",
+                           "row" : null,
+                           "column-number" : 4
+                        },
+                        {
+                          "processor": "goodtables",
+                           "row" : [
+                              "4",
+                              "Salad",
+                              "3"
+                           ],
+                           "message" : "Row 5 has a missing value in column 4",
+                           "column-number" : 4,
+                           "code" : "missing-value",
+                           "row-number" : 5
+                        },
+                        {
+                          "processor": "goodtables",
+                           "column-number" : 5,
+                           "message" : "Row 5 has a missing value in column 5",
+                           "row" : [
+                              "4",
+                              "Salad",
+                              "3"
+                           ],
+                           "row-number" : 5,
+                           "code" : "missing-value"
+                        }
+                     ],
+                     "format" : "csv",
+                     "row-count" : 5,
+                     "valid" : false,
+                     "headers" : [
+                        "ID",
+                        "Mean",
+                        "N",
+                        "Mean",
+                        "Standard Deviation"
+                     ],
+                     "error-count" : 3
+                  }
+               ],
+               "preset" : "table",
+               "valid" : false,
+               "time" : 0.035
+            }'
         ]);
         $report->owner()->associate($dataOwner);
         $report->save();
 
         $report = Report::firstOrNew([
-            'name' => 'Data Profile Name [test]',
+            'name' => 'Data Profile Name [test3]',
         ]);
         $report->fill([
             'errors' => '7',
@@ -57,7 +179,7 @@ class ReportsTableSeeder extends Seeder
         $report->save();
 
         $report = Report::firstOrNew([
-            'name' => 'Data Profile Name [test]',
+            'name' => 'Data Profile Name [test4]',
         ]);
         $report->fill([
             'errors' => '50',
@@ -70,7 +192,7 @@ class ReportsTableSeeder extends Seeder
         $report->save();
 
         $report = Report::firstOrNew([
-            'name' => 'Data Profile Name [test]',
+            'name' => 'Data Profile Name [test5]',
         ]);
         $report->fill([
             'errors' => '7',
