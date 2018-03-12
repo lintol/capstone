1f9d93f3385526f276050ebf159db3e8e3ed791c
Phil Weir <phil.weir@flaxandteal.co.uk>
2018-03-12 20:33:35 +0000

    check for logged in user before trying to return a default provider

--- packages/lintol/capstone/src/ResourceManager.php
+++ packages/lintol/capstone/src/ResourceManager.php
@@ -58,19 +58,21 @@ class ResourceManager
     {
         $currentUser = Auth::user();
 
-        $remoteUser = $currentUser->primaryRemoteUser;
-
         $resourceProvider = null;
 
-        if ($remoteUser) {
-            $resourceable = $remoteUser->resourceable;
+        if ($currentUser) {
+            $remoteUser = $currentUser->primaryRemoteUser;
 
-            if ($resourceable) {
-                switch ($remoteUser->driver) {
-                    case 'ckan':
-                        $resourceProvider = CkanResourceProvider::generate($resourceable, $remoteUser);
-                        break;
-                    default:
+            if ($remoteUser) {
+                $resourceable = $remoteUser->resourceable;
+
+                if ($resourceable) {
+                    switch ($remoteUser->driver) {
+                        case 'ckan':
+                            $resourceProvider = CkanResourceProvider::generate($resourceable, $remoteUser);
+                            break;
+                        default:
+                    }
                 }
             }
         }
