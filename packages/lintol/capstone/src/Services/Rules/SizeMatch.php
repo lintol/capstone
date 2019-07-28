653852a9c670561637d825f74159a06e5f32e0eb
Phil Weir <phil.weir@flaxandteal.co.uk>
2019-07-28 03:55:50 +0100

    not correctly filtering by max size

--- packages/lintol/capstone/src/Services/Rules/SizeMatch.php
+++ packages/lintol/capstone/src/Services/Rules/SizeMatch.php
@@ -7,6 +7,7 @@ class SizeMatch
     public function apply(array $metadata, array $rules)
     {
         if (array_key_exists('maxSize', $rules)) {
+            return true; // FIXME: not correctly filtering
             if (array_key_exists('size', $metadata)) {
                 return $metadata['size'] && $metadata['size'] < $rules['maxSize'];
             }
